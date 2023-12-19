79cc6debe589f372f4ec8894d16c68479a97f4d2
Keith Falcon <kfalcon@lyft.com>
2023-12-18 19:39:37 -0500

    tree-min-value

---
+++ python/tree-min-value.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self, val) -> None:
+        self.val = val
+        self.left = None
+        self.right = None
+        
+a = Node(1)
+b = Node(6)
+c = Node(0)
+d = Node(3)
+e = Node(-6)
+f = Node(2)
+g = Node(2)
+h = Node(2)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+#      1
+#    /   \
+#   6     0
+#  / \     \
+# 3   -6    2
+#    /       \
+#   2         2
+
+def tree_min_value(root):
+    # traverse depth first
+    if root is None:
+        return 0
+    min_value = float('inf')
+    stack = [ root ]
+    while stack:
+        curr = stack.pop()
+        if curr.val < min_value:
+            min_value = curr.val
+        # print(curr.val)
+        if curr.right is not None:
+            stack.append(curr.right)
+        if curr.left is not None:
+            stack.append(curr.left)
+    
+    return min_value
+
+# def depth_first_values(root):
+#     # traverse depth first
+#     if root is None:
+#         return []
+    
+#     left_values = depth_first_values(root.left) # [ b, d, e ]
+#     right_values = depth_first_values(root.right) # [ c, f ]
+    
+#     return [ root.val, *left_values, *right_values]
+    
+
+
+print(tree_min_value(a)) # -> 10
+
