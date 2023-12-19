889ff8056304519c08968ec17bbec8b520f70ddc
Keith Falcon <kfalcon@lyft.com>
2023-12-18 20:32:51 -0500

    max-path-sum

---
+++ python/max_path_sum.py
@@ -0,0 +1,46 @@
+# Write a function, max_path_sum, that takes in the root of a binary tree that contains number values. 
+# The function should return the maximum sum of any root to leaf path within the tree.
+# You may assume that the input tree is non-empty.
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+
+def max_path_sum(root):
+    # traverse depth first
+    if root is None:
+        return float('-inf')
+    
+    if root.left is None and root.right is None:
+        return root.val
+    
+    max_left = max_path_sum(root.left)
+    max_right = max_path_sum(root.right)
+    
+    return root.val + max(max_left, max_right)
+    
+    
+print(max_path_sum(a)) # -> 18
